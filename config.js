6b5ea7ed9c83ebf008ccc94c3825cb86191cfbdc
Mohcine SEDOUANE <MouhcineS@algoconsulting.net>
2022-09-05 10:56:16 +0100

    db config

--- config.js
+++ config.js
@@ -8,10 +8,11 @@ const config = {
   // },
   db: {
     /* don't expose password or any sensitive info, done only for demo */
-    host: "youthful-goldwasser.167-86-124-118.plesk.page",
-    user: "amnays",
-    password: "TLS/@2021",
-    database: "amnays",
+    host: "db-mysql-lon1-84758-do-user-12377163-0.b.db.ondigitalocean.com",
+    user: "doadmin",
+    password: "AVNS_sc_G6AQgj0JsXPxQ2f4",
+    database: "defaultdb",
+    port:"25060"
   },
   listPerPage: 8,
 };
